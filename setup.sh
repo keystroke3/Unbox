e3d4d7d626f4e080b1dff61a510f3eb1bdd32da5
Teddy Okello <37796862+keystroke3@users.noreply.github.com>
2023-02-09 14:27:26 +0300

    Suppress sudo debian interactive prompt; create ~/.zsh dir

--- setup.sh
+++ setup.sh
@@ -2,46 +2,36 @@
 # Assuming running on Ubuntu/Debian
 # This script should be run with sudo
 
-sudo apt update && sudo apt upgrade
-packages=("wget" "zip" "unzip" "zsh" "nginx" "certbot" "python3-certbot-nginx" "postgresql" "ripgrep" "fd-find" "fzf" "python3-virtualenv" "sqlite3" "webp" "redis")
-for pkg in ${packages[@]}
-do
-	sudo apt install -y $pkg
-done
-if $(which exa &> /dev/null); then :; else
-	echo "Exa not found. Installing..."
-	# Install Exa
-	sudo mkdir -p /usr/local/share/zsh/site-functions/
-	wget https://github.com/ogham/exa/releases/download/v0.10.0/exa-linux-x86_64-v0.10.0.zip -O /tmp/exa.zip
-	unzip /tmp/exa.zip 
-	sudo mv bin/exa /usr/local/bin/
-	sudo mv completions/exa.zsh /usr/local/share/zsh/site-functions/
-	sudo mv man/* /usr/share/man/man1/ 
-	rmdir bin completions man
-fi
-
-dots=(".aliases" ".vim" ".zshrc" ".zsh")
+sudo DEBIAN_FRONTEND=noninteractive apt update && sudo DEBIAN_FRONTEND=noninteractive apt upgrade
+packages=("zsh" "nginx" "certbot" "python3-certbot-nginx" "exa" "postgresql" "ripgrep" "fd-find" "fzf" "python3-virtualenv" "sqlite3" "webp" "redis" "unzip" "net-tools" "python3-pip" "libpangocairo-1.0-0")
+dots=(".aliases" ".vim" ".zshrc")
 user_home=$(getent passwd $SUDO_USER | cut -d: -f6)
 echo '***** Setting up ZSH ****'
 for dot in ${dots[@]}
 do
 	ln -s "$(pwd)/$dot" "$user_home/$dot"
+    sudo chown $SUDO_USER:$SUDO_USER $dot
 done
-sudo cp -r .vim /root/
-[ ! -d $user_home/.zsh ] && mkdir $user_home/.zsh
-git clone https://github.com/zsh-users/zsh-autosuggestions $user_home/.zsh/zsh-autosuggestions
-git clone https://github.com/MichaelAquilina/zsh-autoswitch-virtualenv.git $user_home/.zsh/zsh-autoswitch-virtualenv
-git clone https://github.com/zsh-users/zsh-syntax-highlighting $user_home/.zsh/zsh-syntax-highlighting
+for pkg in ${packages[@]}
+do
+	sudo DEBIAN_FRONTEND=noninteractive apt-get install -y $pkg
+done
+echo '***** Setting up ZSH ****'
+zdir="$user_home/.zsh"
+[ -d $zdir ] && rm $zdir
+mkdir $zdir
+echo "$user_home" > "$zdir/lastdir"
+git clone https://github.com/zsh-users/zsh-autosuggestions $zdir/zsh-autosuggestions
+git clone https://github.com/MichaelAquilina/zsh-autoswitch-virtualenv $zdir/zsh-autoswitch-virtualenv
+git clone https://github.com/zsh-users/zsh-syntax-highlighting $zdir/zsh-syntax-highlighting
+sudo chown $SUDO_USER:$SUDO_USER $user_home/.zsh -R
 sudo sh -c "$(curl -fsSL https://starship.rs/install.sh)"
 sudo usermod -s /bin/zsh $SUDO_USER
+sudo chown -R $SUDO_USER:$SUDO_USER $user_home
 echo "Enter new hostname or leave blank to keep $HOST"
 read newhost
 if [ ! -z $newhost ]
 then
 	sudo echo $newhost > /etc/hostname
 fi
-
-echo "Set up complete. Reboot your server to apply any pending changes"
-
-
 
